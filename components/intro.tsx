d9ebdd5c263e0fe73e4fb91b4cba81bdae0055cf
pranav-k-jha <pranav.jha@mail.concordia.ca>
2022-10-09 22:42:50 -0400

    updated intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
         <a
           className="bg-slate-200 group px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer
           borderBlack"
-          href="/CV.pdf"
+          href="https://drive.google.com/file/d/13AQZXkZCyRVHBEqJYK0MZrPOPPRT8R8e/view?usp=sharing"
           download
         >
           Download CV{" "}
